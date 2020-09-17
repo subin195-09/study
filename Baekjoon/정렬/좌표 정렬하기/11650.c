62e25a38a907ab6b83991d8b0acbba375d49c548
soobin <k_subin@naver.com>
2020-09-17 21:52:58 +0900

    09/17 study

---
+++ Baekjoon/정렬/좌표 정렬하기/11650.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    int x;
+    int y;
+} POINT;
+
+int compare(const void *a, const void *b)
+{
+    POINT num1 = *(POINT *)a;
+    POINT num2 = *(POINT *)b;
+
+    if (num1.x < num2.x)
+        return -1;
+    if (num1.x > num2.x)
+        return 1;
+    if (num1.x == num2.x)
+    {
+        if(num1.y < num2.y)
+            return -1;
+        else if(num1.y > num2.y)
+            return 1;
+    }
+    return 0;    
+}
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    POINT p[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d %d", &p[i].x, &p[i].y);
+    
+    qsort(p, n, sizeof(POINT), compare);
+    
+    for(int i = 0; i < n; i++)
+        printf("%d %d\n", p[i].x, p[i].y);
+}
